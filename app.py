35ee5ba7b76d8dd33da561fcd09a1bec49f9b828
rohitp1995 <rohit050695@gmail.com>
2021-02-06 18:28:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,10 @@ def main():
 
     if flask.request.method == 'POST':
         g_name = flask.request.form['game_name']
-        print(g_name)
         if not check_game(g_name):
             return(flask.render_template('not found.html',name=g_name))
         else:
             names = get_recommendation(g_name, no_of_recommendation+1)
-            print(names)
             return flask.render_template('found.html',game_names=names)
 
 
