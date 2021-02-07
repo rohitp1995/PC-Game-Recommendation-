40e0590199cad4934b9b409d5ce7225e2c737768
rohitp1995 <rohit050695@gmail.com>
2021-02-07 12:25:28 +0530

    Update model.py

--- model.py
+++ model.py
@@ -34,24 +34,43 @@ game_8= pd.read_csv("./DATA/game_8.csv")
 game_9= pd.read_csv("./DATA/game_9.csv")
 game_10= pd.read_csv("./DATA/game_10.csv")
 game_11= pd.read_csv("./DATA/game_11.csv")
+
 game_comm=pd.concat([game_0,game_1,game_2,game_3,game_4,game_5,game_6,game_7,game_8,game_9,game_10,game_11],0)
+
 game_info=game_info.iloc[:,1:]
+
 game_comm=game_comm.iloc[:,2:]
+
 df=game_comm.merge(game_info,on='Title')
+
 df=df.dropna()
+
 df=df[['Title','Username','Metascore','Userscore']]
+
 mean_data=pd.DataFrame(df.groupby('Title')['Userscore'].mean().reset_index())
+
 mean_dict=dict(sorted(mean_data.values.tolist())) 
+
 df['Userscore_mean']=df['Title'].map(mean_dict)
+
 score_count=pd.DataFrame(df.groupby('Title')['Userscore_mean'].count().sort_values(ascending=False).reset_index().rename(columns = {'Userscore_mean': 'Userscore_count'}))
+
 rating_with_count = df.merge(score_count, left_on = 'Title', right_on = 'Title', how = 'left')
+
 UserCountplusScore = df.merge(score_count, left_on = 'Title', right_on = 'Title', how = 'left')
+
 count_threshold = 100
+
 rating_popular_game= UserCountplusScore.query('Userscore_count >= @count_threshold')
+
 game_features_df=rating_popular_game.pivot_table(index='Title',columns='Username',values='Userscore_mean').fillna(0)
+
 game_features_df_matrix = csr_matrix(game_features_df.values)
+
 model_knn = NearestNeighbors(metric = 'cosine', algorithm = 'brute')
+
 model_knn.fit(game_features_df_matrix)
+
 all_games = game_features_df.index.to_list()
 
     
